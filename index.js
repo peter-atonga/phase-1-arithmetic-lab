c0f8205879950802a5f2bb97b213a01b6b39488d
peter atonga <peteratonga@gmail.com>
2024-06-28 17:22:11 +0300

    you

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1=31;
+let num2=2;
+let multiply=num1*num2;
 
-// Write your code here
+let random=Math.ceil(Math.random());
+
+let num3=17;
+let num4=13;
+let mod=num3%num4;
+console.log(mod);
+
+let max = Math.max(12,13,17,20)
